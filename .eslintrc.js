57caffc674a85f3e3ec6eb14e045bd9469d86b42
Lorrana <lorrana.neves@buser.com.br>
2023-01-18 22:08:36 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: '',
+    env: {
+        browser:true,
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential', 'eslint:recommended'
+    ],
+    rules: {
+        'vue/multi-word-component-names': 'off'
+    },
+}
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Life></Life>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Life from './components/Life.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Life
   }
 }
 </script>
